fce032dd07868bed0441320ac9506ba768945625
Darwing <darwingd.mosqueraa@ecci.edu.co>
2025-01-06 22:11:53 -0500

    Main section done. Starting with the testing.

--- src/main/java/com/darwing/Main.java
+++ src/main/java/com/darwing/Main.java
@@ -1,5 +1,7 @@
 package com.darwing;
 
+import com.darwing.model.CurrencyConverter;
+
 import java.util.Scanner;
 
 public class Main {
@@ -16,19 +18,39 @@ public class Main {
         System.out.println("------------------------------------");
         System.out.println("|   Welcome to Currency Converter   |");
         System.out.println("------------------------------------");
-        System.out.println("Enter the currency you wish to exchange: ");
-        currencyOrigin = scan.nextLine().toUpperCase();
 
-        System.out.println("Enter the currency you wish to convert to: ");
-        currencyToConvert = scan.nextLine().toUpperCase();
+        while (true) {
+            System.out.println("Enter the currency you wish to exchange: ");
+            currencyOrigin = scan.nextLine().toUpperCase();
 
-        System.out.println("Enter the amount you wish to convert to: ");
-        amount = scan.nextDouble();
+            System.out.println("Enter the currency you wish to convert to: ");
+            currencyToConvert = scan.nextLine().toUpperCase();
 
+            System.out.println("Enter the amount you wish to convert to: ");
+            amount = scan.nextDouble();
 
+            scan.nextLine();
+
+            CurrencyConverter converter = new CurrencyConverter(apiKey);
+            result = converter.convertCurrency(currencyOrigin, currencyToConvert, amount);
+            if (result != -1) {
+                System.out.printf("%.2f %n is: %.2f %s%n", amount, currencyOrigin, result, currencyToConvert);
+            } else {
+                System.out.println("Unable to do the conversion. Verified the data entered and try again.");
+            }
+
+            System.out.print("¿Do you want to make another conversion? (Y/N): ");
+            String continueOption = scan.nextLine().toLowerCase();
+            if (!continueOption.equals("s")) {
+                break;
+            }
+        }
+
+        System.out.println("¡Thanks for using our 'Currency Converter' app. See you soon!");
         scan.close();
-
     }
+}
+
 //
 //
 //    HttpClient client = HttpClient.newHttpClient();
@@ -65,4 +87,3 @@ public class Main {
 ////    }
 //
 //}
-}
